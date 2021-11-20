1081c47c3b04b850344f27b07e6bd00535500c15
hatemets <mistermark009@gmail.com>
2021-11-20 11:14:31 +0200

    Restructure CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './index.css';
 
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
@@ -30,14 +29,14 @@ const App = () => (
 					We hope you had a nice shower!
 				</Card.Text>
 
-				<Row style={{ lineHeight: 1.2 }}>
+				<Row className="line-tight">
 					<Col>
 						<Card.Text style={{ marginBottom: 0 }}>
 							Time spent
 						</Card.Text>
 
 						<Card.Text style={{ color: "red" }}>
-							<span style={{ fontSize: 32, fontWeight: "bold", color: "black" }}>15</span>min
+							<span className="showcase-text">15</span>min
 						</Card.Text>
 					</Col>
 
@@ -47,29 +46,29 @@ const App = () => (
 						</Card.Text>
 
 						<Card.Text style={{ color: "red" }}>
-							<span style={{ fontSize: 32, fontWeight: "bold", color: "black" }}>25</span>min
+							<span className="showcase-text">25</span>min
 						</Card.Text>
 					</Col>
 				</Row>
 
-				<Row style={{ lineHeight: 1.2, marginTop: 10 }}>
+				<Row className="line-tight" style={{ marginTop: 10 }}>
 					<Col>
-						<Card.Text style={{ fontSize: 15, marginBottom: 0 }}>
+						<Card.Text className="margin-bottom-zero" style={{ fontSize: 15 }}>
 							Water Consumption
 						</Card.Text>
 
-						<Card.Text style={{ color: "red" }}>
-							<span style={{ fontSize: 32, fontWeight: "bold", color: "black" }}>65</span>L
+						<Card.Text className="red-text">
+							<span className="showcase-text">65</span>L
 						</Card.Text>
 					</Col>
 
 					<Col >
-						<Card.Text style={{ marginBottom: 0 }}>
+						<Card.Text className="margin-bottom-zero">
 							Regional average
 						</Card.Text>
 
-						<Card.Text style={{ color: "red" }}>
-							<span style={{ fontSize: 32, fontWeight: "bold", color: "black" }}>85</span>L
+						<Card.Text className="red-text">
+							<span className="showcase-text">85</span>L
 						</Card.Text>
 					</Col>
 				</Row>
@@ -80,20 +79,19 @@ const App = () => (
 			<Col style={{ paddingRight: 7}}>
 				<Card>
 					<Card.Body>
-						<Card.Title style={{ marginBottom: 0, fontWeight: "bold", fontSize: 20 }}>Leaderboard Position</Card.Title>
-						<Card.Text style={{ textAlign: "center", color: "red" }}>
-							<span style={{ fontSize: 48, fontWeight: "bold", color: "black" }}>12</span>th
+						<Card.Title className="margin-bottom-zero" style={{ fontWeight: "bold", fontSize: 20 }}>Leaderboard Position</Card.Title>
+						<Card.Text className="center-text red-text">
+							<span className="showcase-text-large">12</span>th
 						</Card.Text>
 					</Card.Body>
 				</Card>
 			</Col>
 			<Col style={{ paddingLeft: 7}}>
-				<Card>
+				<Card style={{ height: "100%" }}>
 					<Card.Body>
-						{/* TODO: Fix hardcoded margin bottom */}	
-						<Card.Title style={{ marginBottom: 4, fontWeight: "bold", fontSize: 18 }}>Most water used at</Card.Title>
-						<Card.Text style={{ textAlign: "center", color: "red" }}>
-							<span style={{ fontSize: 48, fontWeight: "bold", color: "black" }}>6</span>PM
+						<Card.Title style={{ fontWeight: "bold", fontSize: 20 }}>Most water used at</Card.Title>
+						<Card.Text className="center-text red-text">
+							<span className="showcase-text-large">6</span>PM
 						</Card.Text>
 					</Card.Body>
 				</Card>
