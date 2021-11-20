7dc06b99246974a8c5c835a6b442e89b83c4f8d7
hatemets <mistermark009@gmail.com>
2021-11-20 11:39:35 +0200

    Improve navbar; add bell icon

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,6 +3,7 @@ import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import { BellFill } from 'react-bootstrap-icons';
 // import Link from 'react-bootstrap/Link'
 
 const Navigation = () =>
@@ -11,6 +12,11 @@ const Navigation = () =>
 		<Navbar className="navbar" collapseOnSelect expand='sm' bg="dark" fixed="top" variant="dark">
 			<Container>
 				<Navbar.Toggle aria-controls='responsive-navbar-nav'></Navbar.Toggle>
+
+				{/* TODO: Add props */}
+				<div style={{ color: "white", fontSize: 20 }}>Dashboard</div>
+				<BellFill style={{ color: "white", transform: "rotate(30deg) scale(1.5)", marginRight: 5 }} />
+
 				<Navbar.Collapse id='responsive-navbar-nav'>
 					<Nav>
 						<Nav.Link href="/">Home</Nav.Link>       
@@ -18,7 +24,6 @@ const Navigation = () =>
 						<Nav.Link href="/VR">Virtual Reality</Nav.Link>
 					</Nav>
 				</ Navbar.Collapse>
-
 			</Container>
 		</Navbar>
 	);
